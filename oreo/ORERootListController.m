7b9659a9a45d7d613ced9608c49e684f496d63a1
Shade-Zepheri <ziroalpha@gmail.com>
2017-04-08 23:38:20 -0500

    Added headerview to prefs

--- oreo/ORERootListController.m
+++ oreo/ORERootListController.m
@@ -6,6 +6,32 @@
     return @"Root";
 }
 
+- (void)viewDidLoad {
+  	[super viewDidLoad];
+
+  	CGRect frame = CGRectMake(0, 0, self.table.bounds.size.width, 127);
+
+  	UIImage *headerImage = [[UIImage alloc]
+  		initWithContentsOfFile:[[NSBundle bundleWithPath:@"/Library/PreferenceBundles/oreo.bundle"] pathForResource:@"OreoHeader" ofType:@"png"]];
+
+  	UIImageView *headerView = [[UIImageView alloc] initWithFrame:frame];
+  	headerView.image = headerImage;
+  	headerView.backgroundColor = [UIColor blackColor];
+  	headerView.contentMode = UIViewContentModeScaleAspectFit;
+  	headerView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+
+  	self.table.tableHeaderView = headerView;
+}
+
+- (void)viewDidLayoutSubviews {
+  	[super viewDidLayoutSubviews];
+
+  	CGRect wrapperFrame = ((UIView *)self.table.subviews[0]).frame;
+  	CGRect frame = CGRectMake(wrapperFrame.origin.x, self.table.tableHeaderView.frame.origin.y, wrapperFrame.size.width, self.table.tableHeaderView.frame.size.height);
+
+  	self.table.tableHeaderView.frame = frame;
+}
+
 - (void)sendEmail {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:ziroalpha@gmail.com?subject=Oreo"]];
 }
